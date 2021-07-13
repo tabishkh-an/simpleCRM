5dc82435e1ed973b3096117694df28651334fd2f
tabish <tabishnahk@gmail.com>
2021-07-13 12:23:03 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contacts/contacts.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { DeleteContactComponent } from './delete-contact/delete-contact.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddContactComponent,
+    EditContactComponent,
+    DeleteContactComponent,
+    ListContactComponent,
+    ViewContactComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactsModule { }
